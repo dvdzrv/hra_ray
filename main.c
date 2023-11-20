187b4682165acb014eef0f20993900d82b364ae5
dvdzrv <david.szomolai@sostrv.sk>
2023-11-20 22:08:11 +0100

    test

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 
 int main()
 {
-    int width = 800, height = 600, ball_size = 100, ball_velocity = 4;
-    float ball_x = width / 2, ball_y = height / 2;
+    int width = 1200, height = 1000, ball_size = 100;
+    float ball_x = width / 2, ball_y = height / 2, ball_velocity = 4;
     SetTargetFPS(60);
     InitWindow(width, height, "Moja Hra");
     while (! WindowShouldClose( ))
     {
         BeginDrawing();
         ball_y += ball_velocity;
+        ball_velocity += 0.2;
 
         ClearBackground(RED);
         DrawCircle(ball_x, ball_y, ball_size, WHITE);
